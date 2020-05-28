cef9f68549b640821fd98e3a8facfced411d4d90
Luciano Freitas <lucianofdz@gmail.com>
2020-05-28 16:20:03 +0200

    Indent

--- src/main/java/com/example/Process.java
+++ src/main/java/com/example/Process.java
@@ -69,13 +69,13 @@ public class Process extends UntypedAbstractActor {
     }
     
     public void onReceive(Object message) throws Throwable {
-	  if (faultProne) {
+          if (faultProne) {
               if (Math.random() < crashProb) {
-		  dead = true;
-	      }
-	  }
+                  dead = true;
+              }
+          }
 
-	  if (dead) return;
+          if (dead) return;
     	
           if (message instanceof Members) {//save the system's info
               Members m = (Members) message;
@@ -88,15 +88,18 @@ public class Process extends UntypedAbstractActor {
                   p.tell(new Read(), getSelf());
           }
           if (message instanceof Read) {//Broadcast read
-              ActorRef 
+              ActorRef sender = getSender();
               Read r = (Read) message;
               if (readBallot >= r.ballot || imposeBallot >= r.ballot) 
-
+                  sender.tell(new Abort(ballot), getSelf());
+              else {
+                  readBallot = r.ballot;
+                  sender.tell(new Gather(ballot, imposeBallot, estimate), getSelf());
+              }
           }
-	  if (message instanceof CrashMsg) {
-	      faultProne = true;
-	  }
-	  
+          if (message instanceof CrashMsg) {
+              faultProne = true;
+          }
     }
 }
 
