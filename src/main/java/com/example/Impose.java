ca4fb12b740a21b579297a8daa343f9ea4e59bb1
Luciano Freitas <lucianofdz@gmail.com>
2020-05-28 17:15:18 +0200

    New message types

---
+++ src/main/java/com/example/Impose.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Impose {
+    public final int ballot;
+    public final int proposal;
+    public Impose(int ballot, int proposal) {
+        this.ballot = ballot;
+        this.proposal = proposal;
+    }
+}
